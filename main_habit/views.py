ed3f3abd4987e8a095c8fd0845b2a31ce18a48e1
ZlayaMorda <tarus01@mail.ru>
2022-05-23 19:11:15 +0300

    base html and css

--- main_habit/views.py
+++ main_habit/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = ["Профиль", "Подборки", "Обратная связь", "Войти"]
 
 
 def main_page(request):
-    return render(request, 'main_habit/index.html', {'menu': menu, 'title': 'Главная страница'})
+    return render(request, 'main_habit/base.html', {'menu': menu, 'title': 'Главная страница'})
